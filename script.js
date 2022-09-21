a78460f77b660e3df13b7e57d4cf4dc8bd515cd2
fabio-silva-souza <silvafabiosouza@hotmail.com>
2022-09-20 21:01:53 -0300

    initial

---
+++ script.js
@@ -0,0 +1,68 @@
+const game = document.getElementById('game');
+ 
+const showPlay = document.createElement('h3');
+const resultPlay = document.createElement('div');
+resultPlay.classList.add('player')
+const resultComputer = document.createElement('div');
+resultComputer.classList.add('computer')
+const counters = document.createElement('div')
+counters.classList.add('counters')
+const counterP = document.createElement('span');
+counterP.innerHTML = 0
+const counterC = document.createElement('span');
+counterC.innerHTML = 0 
+
+counters.append(counterP,counterC)
+game.append(showPlay,counters,resultPlay,resultComputer);
+
+let choices = [rock = '✊', paper = '✋', scissors = '✌️']
+let player
+let computer
+
+
+
+
+const handleClick = (e) => {
+  player = e.target.id
+  resultPlay.innerHTML = player
+  computerChoice()
+  showResult()
+  // timer here
+}
+
+const computerChoice = () => {
+  const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+  computer = randomChoice
+  resultComputer.innerHTML = computer
+}
+
+for (let i = 0; i < choices.length; i++) {
+  const button = document.createElement('button')
+  button.classList.add('btn-choices')
+  button.id = choices[i]
+  button.innerHTML = choices[i]
+  button.addEventListener('click', handleClick)
+  game.appendChild(button)
+}
+
+const showResult = () => {
+  switch (player + computer) {
+      case scissors + paper:
+          case rock + scissors:
+              case paper + rock:
+                  showPlay.innerHTML = 'You Win!'
+                  counterP.innerHTML ++
+                  break
+      case paper + scissors:
+          case scissors + rock:
+              case rock + paper:
+                  showPlay.innerHTML = 'You Lose Broo!'
+                  counterC.innerHTML ++
+                  break
+      case paper + paper:
+          case scissors + scissors:
+              case rock + rock:
+                  showPlay.innerHTML = 'Its a draw!'
+                  break
+  }
+}
